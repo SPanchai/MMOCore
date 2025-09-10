b69db808d64387703199d67f55ce4891b2f5131c
SettapongPanchai <121502640+SettapongPanchai@users.noreply.github.com>
2025-09-11 01:03:32 +0700

    -fix health bugs

--- src/main/java/net/hyperion/mMOCore/MMOCore.java
+++ src/main/java/net/hyperion/mMOCore/MMOCore.java
@@ -7,15 +7,13 @@ import net.hyperion.mMOCore.database.IDataSource;
 import net.hyperion.mMOCore.database.YamlDataSource;
 import net.hyperion.mMOCore.listeners.MobKillListener;
 import net.hyperion.mMOCore.listeners.PlayerConnectionListener;
-import net.hyperion.mMOCore.listeners.PlayerDamageListener; // <-- IMPORT
-import net.hyperion.mMOCore.listeners.VanillaRegenListener;
+import net.hyperion.mMOCore.listeners.PlayerDamageListener;
 import net.hyperion.mMOCore.listeners.PlayerStatusListener;
-import net.hyperion.mMOCore.stats.StatManager; // <-- IMPORT
+import net.hyperion.mMOCore.listeners.VanillaRegenListener;
+import net.hyperion.mMOCore.mechanics.RegenerationManager; // 1. IMPORT
 import net.hyperion.mMOCore.scoreboard.ScoreboardManager;
+import net.hyperion.mMOCore.stats.StatManager;
 import net.hyperion.mMOCore.ui.ActionBarManager;
-
-
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MMOCore extends JavaPlugin {
@@ -24,36 +22,38 @@ public final class MMOCore extends JavaPlugin {
     private PlayerManager playerManager;
     private IDataSource dataSource;
     private StatManager statManager;
-    private ScoreboardManager scoreboardManager;
     private ActionBarManager actionBarManager;
+    private ScoreboardManager scoreboardManager;
+    private RegenerationManager regenerationManager; // 2. DECLARE
+
     @Override
     public void onEnable() {
         instance = this;
         this.playerManager = new PlayerManager();
         this.statManager = new StatManager();
-        // Initialize the data source
+        this.actionBarManager = new ActionBarManager();
+        this.scoreboardManager = new ScoreboardManager();
+        this.regenerationManager = new RegenerationManager(this); // 3. INITIALIZE
+
         this.dataSource = new YamlDataSource(this);
         this.dataSource.init();
-        this.scoreboardManager = new ScoreboardManager();
-        this.actionBarManager = new ActionBarManager();
 
         // Register event listeners
         getServer().getPluginManager().registerEvents(new PlayerConnectionListener(this), this);
         getServer().getPluginManager().registerEvents(new MobKillListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerDamageListener(this), this);
-        getServer().getPluginManager().registerEvents(new VanillaRegenListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerStatusListener(), this);
+        getServer().getPluginManager().registerEvents(new VanillaRegenListener(), this);
+
         // Register commands
         getCommand("mmocore").setExecutor(new MMOCoreCommand(this));
         getCommand("attribute").setExecutor(new AttributeCommand(this));
-
 
         getLogger().info("MMOCore has been enabled successfully!");
     }
 
     @Override
     public void onDisable() {
-        // Save all online player data on shutdown
         if(getPlayerManager() != null && getPlayerManager().getOnlinePlayers() != null){
             getPlayerManager().getOnlinePlayers().values().forEach(dataSource::savePlayer);
         }
@@ -64,14 +64,15 @@ public final class MMOCore extends JavaPlugin {
     }
 
     // Static accessor for the API
-    public static MMOCore getInstance() { return instance; }
+    public static MMOCore getInstance() {
+        return instance;
+    }
 
     // Accessors
     public PlayerManager getPlayerManager() { return playerManager; }
     public IDataSource getDataSource() { return dataSource; }
     public StatManager getStatManager() { return statManager; }
+    public ActionBarManager getUiManager() { return actionBarManager; }
     public ScoreboardManager getScoreboardManager() { return scoreboardManager; }
-    public ActionBarManager getUiManager() { // <-- 4. RENAME GETTER (or getActionBarManager)
-        return actionBarManager;
-    }
+    public RegenerationManager getRegenerationManager() { return regenerationManager; } // 4. ADD GETTER (optional but good practice)
 }
